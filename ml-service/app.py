2189cd68bb8aedaef2958834e47d3f7f2817deaa
makiii-05 <mdalerta1987qc@student.fatima.edu.ph>
2026-05-03 12:58:17 +0800

    fix: update flask app for deployment

--- ml-service/app.py
+++ ml-service/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes.prediction_routes import prediction_bp
+import os
 
 app = Flask(__name__)
 app.register_blueprint(prediction_bp)
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
